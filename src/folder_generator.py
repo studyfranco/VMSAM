2b01521b314def334007d34e1ed0c26262c0f6b7
studyfranco <studyfranco@gmail.com>
2023-02-16 21:29:18 +0100

    adjust the prepared files

--- src/folder_generator.py
+++ src/folder_generator.py
@@ -6,6 +6,7 @@ Created on 22 Jan 2023
 
 import argparse
 import os
+import shutil
 
 def make_dirs(d):
     try:
@@ -19,12 +20,21 @@ if __name__ == '__main__':
     parser.add_argument("-o","--out", metavar='outdir', type=str, default=".", help="Folder where send new files")
     parser.add_argument("--noSync", dest='noSync', default=False, action='store_true', help="If you don't want research a audio sync between files")
     parser.add_argument("-n","--number", metavar='number', type=int, default=1, help="number of folder you want create")
+    parser.add_argument("--param", metavar='param', type=str, default=None, help="Give the path to a special file for your merge. It prepare it")
+    parser.add_argument("--begin", metavar='begin', type=str, default="", help="Folder begin name")
     args = parser.parse_args()
     if (not os.access(args.out, os.W_OK)):
         raise Exception(f"{args.out} not writable")
+    copy_param = args.param != None
     if args.noSync:
         for i in range(0,args.number):
-            make_dirs(os.path.join(args.out,str(i+1)+" [merge]"))
+            new_folder = os.path.join(args.out,args.begin+str(i+1)+" [merge]")
+            make_dirs(new_folder)
+            if copy_param:
+                shutil.copyfile(args.param, os.path.join(new_folder,"param.json"))
     else:
         for i in range(0,args.number):
-            make_dirs(os.path.join(args.out,str(i+1)))
+            new_folder = os.path.join(args.out,args.begin+str(i+1))
+            make_dirs(new_folder)
+            if copy_param:
+                shutil.copyfile(args.param, os.path.join(new_folder,"param.json"))
