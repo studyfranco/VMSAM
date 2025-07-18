532373239275a589ede131e72b51bba0b8e67a9b
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-07-18 01:37:39 +0000

    fix: correct conditional syntax in run.sh for gestionar_show option

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 if [[ "${software}" == "main" ]]; then
     python3 /home/vmsam/infinite_wrapper.py --pwd /home/vmsam --folder /config/input -o /config/output -e /config/error -c $CORE -w $WAIT
-else if [[ "${software}" == "gestionar_show" ]]; then
+elif [[ "${software}" == "gestionar_show" ]]; then
     python3 /home/vmsam/gestionar_show.py --pwd /home/vmsam --folder /config/input -e /config/error -c $CORE -w $WAIT
 else
     echo "Unknown software option: ${software}"
