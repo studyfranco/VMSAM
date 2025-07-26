ada7ea0362bbee94587c7c21e3b1680b97e116ee
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-07-27 00:25:15 +0200

    this is better

--- src/video.py
+++ src/video.py
@@ -91,7 +91,11 @@ class video():
                         if language_iso_1 != None and language_iso_1 != '':
                             language = language_iso_1
                         else:
-                            language = data['Language'].split("-")[0]
+                            language_iso_1 = Lang(data['Language']).macro().pt1
+                            if language_iso_1 != None and language_iso_1 != '':
+                                language = language_iso_1
+                            else:
+                                language = data['Language'].split("-")[0]
                     else:
                         language = data['Language'].split("-")[0]
                 else:
