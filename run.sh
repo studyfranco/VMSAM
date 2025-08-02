affa9f9923ee47393c24c0795753ccf65296c42d
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-08-02 11:38:26 +0000

    remove stdout buffering

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ "${software}" == "main" ]]; then
-    python3 /home/vmsam/infinite_wrapper.py --pwd /home/vmsam --folder /config/input -o /config/output -e /config/error -c $CORE -w $WAIT
+    python3 -u /home/vmsam/infinite_wrapper.py --pwd /home/vmsam --folder /config/input -o /config/output -e /config/error -c $CORE -w $WAIT
 elif [[ "${software}" == "gestionar_show" ]]; then
-    python3 /home/vmsam/gestionar_show.py --pwd /home/vmsam --folder "${folder_to_watch}" -e ${folder_error} -c $CORE -w $WAIT --database_url_file /database_url.json
+    python3 -u /home/vmsam/gestionar_show.py --pwd /home/vmsam --folder "${folder_to_watch}" -e ${folder_error} -c $CORE -w $WAIT --database_url_file /database_url.json
 else
     echo "Unknown software option: ${software}"
     exit 1
