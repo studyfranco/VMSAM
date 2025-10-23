58c9013904bb29013a8b41408306bfe113cfafc7
Francois STUDER <31127324+studyfranco@users.noreply.github.com>
2025-10-23 22:58:52 +0200

    increase analyse duration

--- src/mergeVideo.py
+++ src/mergeVideo.py
@@ -1488,7 +1488,7 @@ def generate_new_file_audio_config(base_cmd,audio,md5_audio_already_added,audio_
 def generate_new_file(video_obj,delay_to_put,ffmpeg_cmd_dict,md5_audio_already_added,md5_sub_already_added,duration_best_video):
     base_cmd = [tools.software["ffmpeg"], "-err_detect", "crccheck", "-err_detect", "bitstream",
                     "-err_detect", "buffer", "-err_detect", "explode",
-                    "-probesize", "50000000",
+                    "-analyzeduration", "100M", "-probesize", "100M",
                     "-threads", "5", "-vn"]
     if delay_to_put > 0:
         base_cmd.extend(["-itsoffset", f"{delay_to_put/Decimal(1000)}", "-i", video_obj.filePath])
